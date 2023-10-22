40ee0c1149ab5e93aee3efb4a710bd79dadd9f82
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2023-10-21 20:14:27 -0400

    add drivers

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import users from '../users.json'
+import Div100vh from 'react-div-100vh'
+
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -17,14 +20,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main style={{fontFamily: "system-ui, Helvetica"}}>
-        <div style={{width: "100vw", height: "100vh", display: "flex", flexDirection: "column", justifyContent: "space-between", backgroundColor: "#000", color: "#fff"}}>
+        <Div100vh style={{width: "100vw", display: "flex", flexDirection: "column", justifyContent: "space-between", backgroundColor: "#000", color: "#fff"}}>
           <p style={{fontWeight: "700", fontSize: 24, width: "100%", textAlign: "center", paddingTop: 8, }}>Who's Riding?</p>
-          <div>
-            <p>ok</p>
-          </div>
-
+            <div style={{width: "100%", alignItems: "center", justifyContent: "center", display: "flex"}}>
+            <div style={{display: "flex", justifyContent: "space-between", width: 220, rowGap: 24, columnGap: 24, flexWrap: "wrap"}}>
+            {users.map((user) => 
+            <div>
+            <img style={{width: 96, height: 96, objectFit: "cover", borderRadius: 8}} src={user.profilePhoto}/>  
+            <p style={{width: "100%", paddingTop: 6, textAlign: "center", fontWeight: 600}}>{user.username}
+            </p>
+            </div>
+            )}
+            </div>
+            </div>
           <div/>
-        </div>
+        </Div100vh>
       </main>
     </>
   )
