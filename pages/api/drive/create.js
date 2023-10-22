c4e80193bc03f779fcd2b86f64e50388a78d30d4
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2023-10-21 22:07:01 -0400

    ok

--- pages/api/drive/create.js
+++ pages/api/drive/create.js
@@ -1,13 +1,17 @@
 import prisma from "@/lib/db";
+import dayjs from "dayjs";
 
 export default async function handler(req, res) {
   const { username, time } = req.body;
+
+  console.log(req.body);
+
   if (!username || !time) {
     return res.status(400).json({ error: "Missing username and/or time" });
   }
 
   const drive = await prisma.drive.create({
-    data: { departureTime: time, driver: { connect: { username } } },
+    data: { departureTime: dayjs(time), driver: { connect: { username } } },
   });
 
   return res.status(200).json(drive);
