ee080825b3a0b9166bc2ac43e18e892b562188fa
Deet <48701612+Deetschoe@users.noreply.github.com>
2023-10-21 21:00:04 -0400

    added opimage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ogImage: "public/art.jpg",
 }
 
+module.exports = nextConfig;
 module.exports = nextConfig
